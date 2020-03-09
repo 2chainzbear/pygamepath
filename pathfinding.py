4d582e22731c2230aef6bb28f18dcad41ec71f2e
2chainzbear <jameswilcock8@gmail.com>
2020-03-09 15:20:57 -0700

    improved and added message box

--- pathfinding.py
+++ pathfinding.py
@@ -1,6 +1,10 @@
 import pygame
 import sys
 import time
+from tkinter import *
+from tkinter import messagebox
+Tk().wm_withdraw() #to hide the main window
+messagebox.showinfo('ok','Welcome to my a star visualization, the start is the blue square the end is the red square. To move the red square hover over a square and press \'Z\' to place walls hold left click to draw and use right click to erase them. Press p to solve the path.')
 #ddd
 window_size = [610,610]
 black = (0,0,0)
@@ -46,23 +50,7 @@ def astar(start,end):
     openlist.append(start_node)
     count = 0
     checkedlist = []
-    #end is an array holding the row and col
-    
-    #need to set the fcost corresponding to each node, perhaps in a
-    #dictionary,
-    #0 to 100
-    #openlist.append(start)for row in range(10):
-    
-    for row in range(len(grid)):
-        for column in range(len(grid)):
-            if grid[row][column] == 3 and not (row == start[0] and column == start[1]):
-                grid[row][column] = 0 
-            if grid[row][column] == 4:
-                grid[row][column] = 0
-            if row == start[0] and column == start[1]:
-                grid[row][column] = 5
-            if row == end[0] and column == end[1]:
-                grid[row][column] = 2
+    eraseprevpath()
     start_time = time.time()
     while len(openlist) > 0 and count < (len(grid)//2)**10:
         #check adjacent squares up down left right for lowest fcost
@@ -99,19 +87,15 @@ def astar(start,end):
             if node_position[0] == start[0] and node_position[1] == start[1]:
                 continue
 
+            #make sure not travelling through "cracks" in the walls
             if new_position in[(1,1),(-1,1),(-1,-1),(1,-1)]:
                 if  -1 < node_position[0]-new_position[0] < len(grid) and -1 < node_position[1]-new_position[1] < len(grid):
                     if grid[node_position[0]-new_position[0]][node_position[1]] == 1 and grid[node_position[0]][node_position[1]-new_position[1]] == 1:
-                            #print("is wall")
-                            #print(node_position[0]-new_position[0],node_position[1]-new_position[1])
+                            
                             continue
-            #print((node_position[0]),(node_position[1]))
-            #print(grid[node_position[0]][node_position[1]]) 
             # Create new node
             new_node = Node(current_node, node_position)
-            #save this for a slow mo path find 
-            #print(node_position[0],node_position[1])
-            grid[node_position[0]][node_position[1]] = 3
+            
             if node_position not in checkedlist:
                 checkedlist.append(node_position)
             for closed_child in closedlist:
@@ -127,36 +111,7 @@ def astar(start,end):
                     continue
 
         count += 1
-        '''
-        screen.fill(black)
         
-        #if grid != previousgrid:  
-        #previousgrid = grid
-        for row in range(len(grid)):
-            for column in range(len(grid)):
-                color = white
-                if grid[row][column] == 1:
-                    color = black
-                if grid[row][column] == 2:
-                    color = red
-                if grid[row][column] == 3 and not (row == start[0] and column == start[1]):
-                    color = lightbluegreen
-                if row == start[0] and column == start[1]:
-                    color = blue
-                if grid[row][column] == 4:
-                    color = lightblue
-                if row == end[0] and column == end[1]:
-                    color = red
-                pygame.draw.rect(screen, 
-                                color,
-                                [(margin+width)*column+margin,
-                                (margin+height)*row+margin,
-                                width,
-                                height])
-        print(len(openlist))
-        pygame.display.flip()
-        time.sleep(.2/count)
-        '''
 
 def eraseprevpath():
     for row in range(len(grid)):
@@ -256,7 +211,7 @@ while not done:
             pos = pygame.mouse.get_pos()
             column = pos[0] // (width+margin)
             row = pos[1] // (height+margin)
-            print("Click " , pos, "Grid coordinates: ", row,column)
+            #print("Click " , pos, "Grid coordinates: ", row,column)
             if row < len(grid) and column < len(grid):
                 if not (row == start[0] and column == start[1]):
                     grid[row][column] = 1
@@ -267,7 +222,7 @@ while not done:
             if row < len(grid) and column < len(grid):
                 if not (row == start[0] and column == start[1]):
                     grid[row][column] = 0
-            print("Click " , pos, "Grid coordinates: ", row,column)
+            #print("Click " , pos, "Grid coordinates: ", row,column)
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_z:
                 pos = pygame.mouse.get_pos()
@@ -289,10 +244,6 @@ while not done:
                 eraseprevpath()
                 data,checkedlist = astar(start,end)
                 doanimation(checkedlist,data)
-                
-                #for x in data:
-                 #   if not x[0] == start[0] and  x[1]== start[0]:
-                  #      grid[x[0]][x[1]] = 4
 
     screen.fill(black)
 
@@ -318,26 +269,5 @@ while not done:
                             (margin+height)*row+margin,
                             width,
                             height])
-    '''
-    for x in range(10):
-        for y in range(10):
-            gcost = 0 + x + 0 + y
-            endx = end[0]
-            endy = end[1]
-            hcost = (9-x)*(9-x) + (9-y)*(9-y)
-            if hcost < 0:
-                hcost *=-1
-            fcost = gcost + hcost
-            fcostforrender = str(fcost).encode("utf-8").decode("utf-8") 
-            smallerfont.render(fcostforrender,True,black)
-            screen.blit(smallerfont.render(fcostforrender,True,(0,0,0)),(x*25+5,y*25+5))
-    '''
-    '''
-    pygame.draw.rect(screen,black,[0,255,255,100])
-    screen.blit(font.render('Press p to compute path from the black',True,(230,0,0)),(10,260))
-    screen.blit(font.render('square to the red, place walls with left click', True,(230,0,0)),(10,280))
-    screen.blit(font.render('and press z while hovering over a grid to place', True,(230,0,0)),(0,300))
-    screen.blit(font.render('the end of the path', True,(230,0,0)),(80,320))
-    '''
     clock.tick(60)
     pygame.display.flip()
